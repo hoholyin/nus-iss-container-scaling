a434c11fe8638c19a0f614f35c341ee4b42a48a1
hoholyin <holyin97@gmail.com>
2024-08-16 22:49:56 +0800

    add frontend files

---
+++ workshop01/frontend/src/messageHolder.js
@@ -0,0 +1,33 @@
+import {useEffect, useState} from "react";
+
+const MessageHolder = (props) => {
+    const [message, setMessage] = useState("");
+
+    const getRequest = async function (url) {
+        const requestOptions = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }
+        const promise = await fetch(url, requestOptions)
+        return await promise.json()
+    }
+
+    useEffect(() => {
+        async function fetchData() {
+            const messageObj = await getRequest("https://nus-iss-container-scaling.onrender.com/api/getMessage");
+            console.log(JSON.stringify(messageObj));
+            setMessage(messageObj.data);
+        }
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            <span>{message}</span>
+        </div>
+    )
+}
+
+export default MessageHolder;
