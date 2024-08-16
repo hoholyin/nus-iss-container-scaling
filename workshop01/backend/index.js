9eb529130d5a315c0bdce5fe498d868ab92a808c
hoholyin <holyin97@gmail.com>
2024-08-17 00:05:37 +0800

    update cors to allow from all origins

--- workshop01/backend/index.js
+++ workshop01/backend/index.js
@@ -14,7 +14,7 @@ if (port == null || port === "") {
 // app.use(cors())
 app.use(express.json())
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", ["https://hoholyin.github.io"]);
+    res.header("Access-Control-Allow-Origin", ["*"]);
     res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header("Access-Control-Allow-Headers", "Content-Type");
     next();
